1be6ac55a4409ad87ac17d6d8da9041ca4385bab
shrutiyer <shruti.iyer@students.olin.edu>
2015-10-22 21:09:15 -0400

    Serial Write without kinect

---
+++ Non_kinect_interaction/serialBinary.py
@@ -0,0 +1,36 @@
+import time
+import serial
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial(
+    port='/dev/ttyACM0',
+    baudrate=9600,
+    timeout=2,
+)
+
+#ser.open()
+ser.isOpen()
+
+print 'Enter your commands below.\r\nInsert "exit" to leave the application.'
+
+input1=1
+while 1 :
+    # get keyboard input
+    input1 = raw_input(">> ")
+        # Python 3 users
+        # input = input(">> ")
+    if input1 == 'exit':
+        ser.close()
+        exit()
+    else:
+        # send the character to the device
+        # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
+        ser.write(input1)
+        out = ''
+        # let's wait one second before reading output (let's give device time to answer)
+        time.sleep(1)
+        #while ser.inWaiting() > 0:
+        #    out += ser.read(1)
+
+        #if out != '':
+        #    print ">>" + out
